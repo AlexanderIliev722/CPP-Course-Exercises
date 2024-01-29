9a49d284537a1e5ca0f503b7e293065aae201229
Alexander Iliev <80001685+AlexanderIliev722@users.noreply.github.com>
2024-01-29 19:52:56 +0200

    Add files via upload

---
+++ Animals/Dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H_INCLUDED
+#define DOG_H_INCLUDED
+
+#include "Animal.h"
+
+class Dog : public Animal
+{
+public:
+	Dog(string, string, char, float, string); //can also: Dog(int, string, string, char); but we have to be careful because we can get it wrong
+	void setBark(string);
+	string getBark();
+	void printInfo(); //override in the Animal printInfo(): so we can see the bark too - we call the parent printInfo() and then add additional information for Dog
+private:
+	string bark;
+
+};
+
+
+#endif // DOG_H_INCLUDED
