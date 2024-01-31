d4327d5be7380be5045911b1b7ba006d6376d4a5
Alexander Iliev <80001685+AlexanderIliev722@users.noreply.github.com>
2024-01-31 22:02:08 +0200

    Add files via upload

---
+++ IzpitExample/Genre.h
@@ -0,0 +1,15 @@
+#ifndef GENRE_H_INCLUDED
+#define GENRE_H_INCLUDED
+
+class Genre{
+public:
+Genre(int);
+void setGenre(int);
+int getGenre();
+void print();
+
+private:
+int genre;
+};
+
+#endif // GENRE_H_INCLUDED
